b83f0c5f2ed36aadc4bc89215a58873487be9329
mskalisz <mskalisz3@gmail.com>
2022-07-24 19:58:46 -0400

    Solve problem 5

---
+++ preparations_exercises/5.js
@@ -0,0 +1,10 @@
+/*
+Identify the Constructors for each of the following methods and classify each method as either a "Static" or a "Prototype" method:
+
+substring: String constructor; instance method
+create: Object constructor; static method
+fromCharCode: String constructor; static method
+slice: String/Array constructor; instance method
+toString: String/Regexp/Array/Object/Function/Boolean/Symbol/Number constructor; instance method
+
+*/
