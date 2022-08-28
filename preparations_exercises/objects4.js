7dafd22ca303bfff85307be8c8f7521558d42ae1
mskalisz <mskalisz3@gmail.com>
2022-08-28 11:52:36 -0400

    Add LS solution

--- preparations_exercises/objects4.js
+++ preparations_exercises/objects4.js
@@ -11,3 +11,9 @@ let upperCasedKeys = []
 console.log(obj)
 console.log(array)
 console.log(upperCasedKeys)
+
+// LS Solution
+// let objKeys = Object.keys(obj);
+// let upperKeys = objKeys.map((key) => key.toUpperCase());
+// console.log(upperKeys); // => [ 'B', 'A', 'C' ]
+// console.log(obj); // => { b: 2, a: 1, c: 3 }
