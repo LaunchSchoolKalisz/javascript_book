b99cc7d17259782275fac208c41eba48fe2b883c
mskalisz <mskalisz3@gmail.com>
2022-08-29 16:32:46 -0400

    Add LS solution

--- preparations_exercises/objects8.js
+++ preparations_exercises/objects8.js
@@ -28,3 +28,19 @@ console.log(newObj2);       // => { foo: 1, qux: 3 }
 
 let newObj3 = copyObj(objToCopy, [ 'bar' ]);
 console.log(newObj3);       // => { bar: 2 }
+
+// LS Solution
+
+// function copyObj(sourceObject, keys) {
+//   let destinationObject = {};
+
+//   if (keys) {
+//     keys.forEach(function(key) {
+//       destinationObject[key] = sourceObject[key];
+//     });
+
+//     return destinationObject;
+//   } else {
+//     return Object.assign(destinationObject, sourceObject);
+//   }
+// }
